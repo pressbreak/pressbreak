7c8b0a3532ec1f62b43d9b1d4c77d2dbad707375
Garrett Grice <ggrice@gmail.com>
2015-07-06 22:07:10 -0700

    Added fontawesome to the app, using it's refresh + fa-spin class for refresh icon on scan partial page

--- app.php
+++ app.php
@@ -30,6 +30,7 @@ Helper::initPage();
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap-theme.min.css">
   <link rel="stylesheet" href="css/app.css" media="screen" charset="utf-8">
+  <link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css">
   <script src="//code.jquery.com/jquery-1.11.3.min.js"></script>
   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
   <script src="vendor/angularjs/angular.min.js"></script>
